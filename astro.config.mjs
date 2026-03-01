d3ee413e43e0742f828cc6f4e5d719399ae4a3e8
Admir <admirfric@gmail.com>
2026-03-01 12:04:03 +0100

    fix deploy error

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,16 +3,12 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 import sitemap from '@astrojs/sitemap';
 import mdx from '@astrojs/mdx';
-import cloudflare from '@astrojs/cloudflare';
 import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://exyuiptv.org',
   output: 'static',
-  adapter: cloudflare({
-    imageService: 'cloudflare'
-  }),
   integrations: [
     react(),
     sitemap(),
